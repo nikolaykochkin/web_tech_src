6c95545fe1fc4da9eb57a6099543b46ce4649ac8
Николай <nikolaykochkin91@gmail.com>
2019-03-30 17:06:31 +0300

    Вторая лаба через форк

--- tcp_server_multi_user.py
+++ tcp_server_multi_user.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 2222))
@@ -6,8 +8,14 @@ s.listen(10)
 
 while True:
     conn, addr = s.accept()
-    data = conn.recv(1024)
-    if not data or data == 'close': break
-    conn.send(data)
+    child_pid = os.fork()
+    if child_pid == 0:
+        data = conn.recv(1024)
+        if not data or data == 'close': break
+        conn.send(data)
+        conn.close()
+        sys.exit()
+    else:
+        conn.close()
 
-    conn.close()
+s.close()

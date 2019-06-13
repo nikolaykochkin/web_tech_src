5d872c1bb5fd205142eab45ac1e586fed77dce3f
nikolaykochkin <43437137+nikolaykochkin@users.noreply.github.com>
2019-06-13 17:50:24 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
-sudo mkdir -p /var/www/web
-sudo chown -R $USER:$USER /var/www/web
-sudo chmod -R 755 /var/www/web
-git clone https://github.com/nikolaykochkin/web_tech_src /var/www/web
-sudo ln -s /var/www/web/etc/nginx.conf /etc/nginx/sites-enabled/test
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

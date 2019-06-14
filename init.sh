435a36da70c12a75de0bb1a460d086d4aad13d87
nikolaykochkin <43437137+nikolaykochkin@users.noreply.github.com>
2019-06-14 12:56:16 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 -w 2 /home/box/web/hello:app

f25c0fbf381cc8b119f773bf95932164f01ff680
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-14 16:57:37 +0300

    Новый конфиг

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart

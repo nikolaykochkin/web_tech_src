45d161c28c80d332e09c9bb6378db5fd0318c92d
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-14 16:30:30 +0300

    Гит игнор

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd /home/box/web
-source venv/bin/activate
-gunicorn -b 0.0.0.0:8080 -w 2 -p /tmp/gunicorn.pid -D hello:app
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart

0653b11d1031f1a6891988e590ec5258dcdefb3c
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-14 14:45:00 +0300

    Инициализация через виртуальную среду

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 -w 2 /home/box/web/hello:app
+cd /home/box/web
+source venv/bin/activate
+gunicorn -b 0.0.0.0:8080 -w 2 -p /tmp/gunicorn.pid -D hello:app

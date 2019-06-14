d1e2f2ae7cfad4122b729a80e54b8a24ba58eba5
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-14 16:43:38 +0300

    Гит игнор

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

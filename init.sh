ee7a7067aa389c2ecc3137c816ba4db55f82f9a6
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-19 18:10:58 +0300

    +++

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
 sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo /etc/init.d/gunicorn restart
+#sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+#sudo /etc/init.d/gunicorn restart

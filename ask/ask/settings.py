a0eae593d85c1aef1104e04e19874bb92ad03e71
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-23 17:37:50 +0300

    СУБД

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'qa.apps.QaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return None
+
+    def popular(self):
+        return None
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_to_likes')
+    object = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+

10ece9a805fd241da856b79e420e5eee94e4e9c9
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-26 18:39:30 +0300

    +++

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test
-sudo mv /etc/nginx/sites-enabled/test /etc/nginx/sites-enabled/default
+
+sudo apt-get update
+sudo apt-get install -y python3.5
+sudo apt-get install -y python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+virtualenv -p python3 venv
+
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 #sudo /etc/init.d/gunicorn restart
---
+++ venv_init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pip install --upgrade pip
+pip install -r req.txt
+gunicorn ask.wsgi -b 0.0.0.0:8000 --pid /tmp/gunicorn1.pid --daemon
+gunicorn hello:app -b 0.0.0.0:8080 --pid /tmp/gunicorn2.pid --daemon

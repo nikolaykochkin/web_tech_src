811f7a20df094cc4fa27ea22ab4033a458142e21
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-26 18:45:22 +0300

    +++

--- init.sh
+++ init.sh
@@ -10,5 +10,10 @@ virtualenv -p python3 venv
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo /etc/init.d/mysql start
+mysql -u root -e "create database stepic_web;"
+mysql -u root -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+
 #sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 #sudo /etc/init.d/gunicorn restart

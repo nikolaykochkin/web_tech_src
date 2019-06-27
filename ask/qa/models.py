499c398b33eb59db11daf889aa4cd86a602a8e86
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-27 10:27:28 +0300

    +++

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,8 +13,8 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_to_likes')
     objects = QuestionManager()
@@ -25,7 +25,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
--- init.sh
+++ init.sh
@@ -12,8 +12,9 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo /etc/init.d/mysql start
-mysql -u root -e "create database stepic_web;"
-mysql -u root -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+mysql -uroot -e "CREATE DATABASE ask;"
+mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '123';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ask.* TO 'box'@'localhost' WITH GRANT OPTION;"
 
 #sudo ln -s /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 #sudo /etc/init.d/gunicorn restart

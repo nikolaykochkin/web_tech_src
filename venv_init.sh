c49003ec9f85d0e1e29f04d979a28c35b0e11838
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-06-28 21:00:15 +0300

    Лаба "Отображение данных"

--- venv_init.sh
+++ venv_init.sh
@@ -2,5 +2,7 @@
 
 pip install --upgrade pip
 pip install -r req.txt
+python ask/manage.py makemigrations
+python ask/manage.py migrate
 gunicorn ask.ask.wsgi -b 0.0.0.0:8000 --pid /tmp/gunicorn1.pid --daemon
 gunicorn hello:app -b 0.0.0.0:8080 --pid /tmp/gunicorn2.pid --daemon

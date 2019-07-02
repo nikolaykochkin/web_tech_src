65e2e3cbd50a66c55c07ee310d678ef28a6cb471
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-07-02 18:13:03 +0300

    forms

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', views.test, name='test'),
     path('signup/', views.test, name='test'),
     path('question/<int:question_id>/', views.detail, name='detail'),
-    path('ask/', views.test, name='test'),
+    path('ask/', views.ask, name='ask'),
     path('popular/', views.popular, name='popular'),
     path('new/', views.test, name='test'),
 ]

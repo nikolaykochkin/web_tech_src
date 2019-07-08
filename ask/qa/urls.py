dac6a1afa8b914c005f83cdb87ca0a321d32c23b
nikolaykochkin <nikolaykochkin91@gmail.com>
2019-07-08 19:15:26 +0300

    forms

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 app_name = 'qa'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.test, name='test'),
+    path('login/', auth_views.LoginView.as_view()),
     path('signup/', views.test, name='test'),
     path('question/<int:question_id>/', views.detail, name='detail'),
     path('ask/', views.ask, name='ask'),
